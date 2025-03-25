109f7a1674f5055564a0c962b144dd574311eef7
Egor Zubenko <zegor88@gmail.com>
2025-03-25 11:22:45 +0100

    README

--- dashboard/callbacks/__init__.py
+++ dashboard/callbacks/__init__.py
@@ -7,5 +7,4 @@ from idadv_dash_simulator.dashboard.simulation import *
 from idadv_dash_simulator.dashboard.callbacks.economy import *
 from idadv_dash_simulator.dashboard.callbacks.progression import *
 from idadv_dash_simulator.dashboard.callbacks.locations import *
-from idadv_dash_simulator.dashboard.callbacks.tapping import *
-# Не импортируем callbacks/simulation.py, т.к. все его функции уже есть в dashboard/simulation.py 
+from idadv_dash_simulator.dashboard.callbacks.tapping import * 
